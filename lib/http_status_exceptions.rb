a50f160359a14df515db745d6abf197de8eebfec
Willem van Bergen <willem@vanbergen.org>
2009-09-26 12:29:02 +0200

    Generate subclasses on demand instead of all on launch, refectored specs to take this intro account.

--- lib/http_status_exceptions.rb
+++ lib/http_status_exceptions.rb
@@ -35,11 +35,14 @@ module HTTPStatus
   # Creates all the exception classes based on Rails's list of available status code and
   # registers the exception handler using the rescue_from method.
   def self.included(base)
-    ActionController::StatusCodes::STATUS_CODES.each do |code, name|
-      const_set(name.to_s.gsub(/[^A-Za-z]/, '').camelize, Class.new(HTTPStatus::Base)) if code >= 400
-    end
-    
     base.send(:rescue_from, HTTPStatus::Base, :with => :http_status_exception)
+  end
+  
+  # Generates a HTTPStatus::Base subclass for every subclass that is found
+  def self.const_missing(const)
+    klass = Class.new(HTTPStatus::Base)
+    const_set(const, klass)
+    return const_get(const)
   end
 
   # The default handler for raised HTTP status exceptions. 
--- spec/http_status_exception_spec.rb
+++ spec/http_status_exception_spec.rb
@@ -1,45 +1,5 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe 'HTTPStatus#http_status_exception' do
-  before(:each) { @controller = Class.new(ActionController::Base).new }
-  after(:each)  { HTTPStatus::Base.template_layout = nil}
-  
-  it "should respond to the :http_status_exception method" do
-    @controller.should respond_to(:http_status_exception)
-  end
-  
-  ['NotFound', 'Forbidden', 'PaymentRequired'].each do |status_class|
-    status_symbol = status_class.underscore.downcase.to_sym
-    
-    it "should create the HTTPStatus::#{status_class} class" do
-      HTTPStatus.const_defined?(status_class).should be_true
-    end
-    
-    it "should create a subclass of HTTPStatus::Base for the #{status_class.underscore.humanize.downcase} status" do
-      HTTPStatus.const_get(status_class).ancestors.should include(HTTPStatus::Base)
-    end
-    
-    it "should call render with the correct #{status_class.underscore.humanize.downcase} view and correct HTTP status" do
-      @controller.should_receive(:render).with(hash_including(
-            :status => status_symbol, 
-            :template => "shared/http_status/#{status_symbol}"))
-
-      @controller.http_status_exception(HTTPStatus.const_get(status_class).new('test'))
-    end
-
-    it "should not call render with a layout by default" do
-      @controller.should_not_receive(:render).with(hash_including(:layout => 'testing'))
-      @controller.http_status_exception(HTTPStatus.const_get(status_class).new('test'))
-    end
-
-    it "should call render with a layout set when this property is set on the exception class" do
-      @controller.should_receive(:render).with(hash_including(:layout => 'testing'))
-      HTTPStatus::Base.template_layout = 'testing'
-      @controller.http_status_exception(HTTPStatus.const_get(status_class).new('test'))
-    end    
-  end
-end
-
 describe HTTPStatus::Base do
   before(:each) do
     @status_exception_class = Class.new(HTTPStatus::Base)
@@ -47,6 +7,8 @@ describe HTTPStatus::Base do
     @status_exception = @status_exception_class.new('Testing the base exception class')
   end
   
+  after(:each) { HTTPStatus::Base.template_path = 'shared/http_status' }
+
   it "should set the status symbol bases on the class name" do
     @status_exception.status.should == :testing_status
   end
@@ -62,3 +24,47 @@ describe HTTPStatus::Base do
   end
 end
 
+# Run some tests for different valid subclasses.
+{ 'NotFound' => 404, 'Forbidden' => 403, 'PaymentRequired' => 402, 'InternalServerError' => 500}.each do |status_class, status_code|
+
+  describe "HTTPStatus::#{status_class}" do
+    it "should generate the HTTPStatus::#{status_class} class successfully" do
+      lambda { HTTPStatus.const_get(status_class) }.should_not raise_error
+    end
+
+    it "should create a subclass of HTTPStatus::Base for the #{status_class.underscore.humanize.downcase} status" do
+      HTTPStatus.const_get(status_class).ancestors.should include(HTTPStatus::Base)
+    end
+
+    it "should return the correct status code (#{status_code})" do
+      HTTPStatus.const_get(status_class).new.status_code.should == status_code
+    end
+  end
+end
+
+describe 'HTTPStatus#http_status_exception' do
+  before(:each) { @controller = Class.new(ActionController::Base).new }
+  after(:each)  { HTTPStatus::Base.template_layout = nil}
+  
+  it "should create the :http_status_exception method in ActionController" do
+    @controller.should respond_to(:http_status_exception)
+  end
+
+  it "should call render with the correct view and correct HTTP status" do
+    @controller.should_receive(:render).with(hash_including(
+          :status => :base, :template => "shared/http_status/base"))
+
+    @controller.http_status_exception(HTTPStatus::Base.new('test'))
+  end
+
+  it "should not call render with a layout by default" do
+    @controller.should_not_receive(:render).with(hash_including(:layout => 'testing'))
+    @controller.http_status_exception(HTTPStatus::Base.new('test'))
+  end
+
+  it "should call render with a layout set when this property is set on the exception class" do
+    @controller.should_receive(:render).with(hash_including(:layout => 'testing'))
+    HTTPStatus::Base.template_layout = 'testing'
+    @controller.http_status_exception(HTTPStatus::Base.new('test'))
+  end
+end
